ca957e453a0bd3ce7385f2a7f5d4cf3ca6b0fa82
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-31 11:41:14 -0300

    charge test

---
+++ tests/ChargeTest.php
@@ -0,0 +1,45 @@
+<?php
+
+class ChargeTest extends PHPUnit_Framework_TestCase
+{
+    public function test_someone_can_be_charged()
+    {
+        $api_key = getenv("IUGU_API_KEY");
+        $account_id = getenv("IUGU_ACCOUNT_ID");
+
+        $iugu = new Iugu($api_key);
+
+        $token = $iugu->token()->create([
+            'account_id' => $account_id,
+            'method' => 'credit_card',
+            'test' => true,
+            'data' => [
+                'number' => '4111111111111111',
+                'verification_value' => '123',
+                'first_name' => 'Joao',
+                'last_name' => 'Silva',
+                'month' => '12',
+                'year' => '2016',
+            ],
+        ]);
+
+        $charge = $iugu->charge()->create([
+            [
+                "token" => $token->id,
+                "email" => "teste@superteste.abc",
+                "items" => [
+                    [
+                        "description" => "Item Teste",
+                        "quantity" => "1",
+                        "price_cents" => "1000",
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertNotNull($charge);
+        $this->assertEquals($charge->success, true);
+        $this->assertEquals($charge->message, 'Autorizado');
+        $this->assertNotNull($charge->invoice_id);
+    }
+}
