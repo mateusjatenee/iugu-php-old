b8832d93b2a8b788b8a91794e47f520ea93b0576
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-31 16:05:35 -0300

    new test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -4,17 +4,19 @@ use Iugu\Iugu;
 
 class ClientTest extends PHPUnit_Framework_TestCase
 {
+    public function __construct()
+    {
+        $this->api_key = getenv("IUGU_API_KEY");
+        $this->account_id = getenv("IUGU_ACCOUNT_ID");
+        $this->iugu = new Iugu($this->api_key);
+    }
     public function test_client_can_be_created()
     {
-        $api_key = getenv("IUGU_API_KEY");
-        $account_id = getenv("IUGU_ACCOUNT_ID");
-
-        $iugu = new Iugu($api_key);
 
         $email = 'email@email.com';
         $name = 'Joao';
 
-        $client = $iugu->customer()->create([
+        $client = $this->iugu->customer()->create([
             'email' => $email,
             'name' => $name,
             'notes' => 'nenhuma',
@@ -23,5 +25,19 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $this->assertNotNull($client);
         $this->assertEquals($client->email, $email);
         $this->assertEquals($client->name, $name);
+    }
+
+    public function test_client_can_be_fetched()
+    {
+        $client = $this->iugu->customer()->create([
+            'email' => 'john@doe.com',
+            'name' => 'John Doe',
+            'notes' => 'None',
+        ]);
+
+        $foundCustomer = $this->iugu->customer()->fetch($client->id);
+
+        $this->assertEquals($foundCustomer->email, $client['email']);
+        $this->assertEquals($foundCustomer->name, $client['name']);
     }
 }
