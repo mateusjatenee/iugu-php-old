41da039c4ec753b9f3b2bac8e4bbbb95a8c83316
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-31 11:43:40 -0300

    constructor

--- src/Charge.php
+++ src/Charge.php
@@ -6,9 +6,16 @@ use Iugu\Util\Request;
 
 class Charge extends Request
 {
+    private $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
     public function create($data)
     {
-        $req = $this->postRequest('charge', $data);
+        $req = $this->postRequest('charge', $data, $this->apiKey);
         return $req;
     }
 }
