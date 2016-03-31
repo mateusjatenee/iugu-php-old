a50bc953a8000eb72119d63e4eb0c5fbad401557
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-31 17:22:14 -0300

    docblock

--- src/Iugu.php
+++ src/Iugu.php
@@ -4,23 +4,41 @@ namespace Iugu;
 
 class Iugu
 {
+    /**
+     * @var string
+     */
     private $apiKey;
 
+    /**
+     * @param $apiKey
+     */
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
     }
 
+    /**
+     * Retorna uma instância da classe Token
+     * @return Iugu\Token
+     */
     public function token()
     {
         return new Token($this->apiKey);
     }
 
+    /**
+     * Retorna uma instância da classe Charge
+     * @return Iugu\Charge
+     */
     public function charge()
     {
         return new Charge($this->apiKey);
     }
 
+    /**
+     * Retorna uma instância da classe Customer
+     * @return Iugu\Customer
+     */
     public function customer()
     {
         return new Customer($this->apiKey);
