1ead167bdc33176f0372be2f9f6ea5fac6f3881c
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-02 05:17:17 -0300

    docblock

--- src/SubAccount.php
+++ src/SubAccount.php
@@ -6,13 +6,23 @@ use Iugu\Util\Request;
 
 class SubAccount extends Request
 {
+    /**
+     * @var string
+     */
     private $apiKey;
 
+    /**
+     * @param $apiKey
+     */
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
     }
 
+    /**
+     * @param $data
+     * @return stdClass
+     */
     public function create($data)
     {
         $req = $this->postRequest('marketplace/create_account', $data, $this->apiKey);
