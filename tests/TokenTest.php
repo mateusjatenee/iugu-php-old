52e7a64aefefd25b073335a31ee336355a1bec3c
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-07 11:33:35 -0300

    @group

--- tests/TokenTest.php
+++ tests/TokenTest.php
@@ -4,7 +4,11 @@ use Iugu\Iugu;
 
 class TokenTest extends PHPUnit_Framework_TestCase
 {
-    public function test_token_can_be_created()
+    /**
+     * @group token
+     */
+
+    public function testTokenCanBeCreated()
     {
         $api_key = getenv('IUGU_API_KEY');
         $account_id = getenv('IUGU_ACCOUNT_ID');
@@ -12,15 +16,15 @@ class TokenTest extends PHPUnit_Framework_TestCase
         $iugu = new Iugu($api_key);
         $token = $iugu->token()->create([
             'account_id' => $account_id,
-            'method'     => 'credit_card',
-            'test'       => true,
-            'data'       => [
-                'number'             => '4111111111111111',
+            'method' => 'credit_card',
+            'test' => true,
+            'data' => [
+                'number' => '4111111111111111',
                 'verification_value' => '123',
-                'first_name'         => 'Joao',
-                'last_name'          => 'Silva',
-                'month'              => '12',
-                'year'               => '2016',
+                'first_name' => 'Joao',
+                'last_name' => 'Silva',
+                'month' => '12',
+                'year' => '2016',
             ],
         ]);
 
