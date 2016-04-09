3bbc398a58fef29bfc1ad2367468c963dbc8a2bb
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-09 19:58:14 -0300

    Transfer test

---
+++ tests/TransferTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class TransferTest extends PHPUnit_Framework_TestCase
+{
+    public function __construct()
+    {
+        $this->api_key = getenv('IUGU_API_KEY');
+        $this->account_id = getenv('IUGU_ACCOUNT_ID');
+        $this->iugu = new Iugu($this->api_key);
+    }
+
+    public function testTransferCanBeDone()
+    {
+        $email = 'email@email.com';
+        $name = 'Joao';
+
+        $client = $this->iugu->customer()->create([
+            'email' => $email,
+            'name' => $name,
+            'notes' => 'nenhuma',
+        ]);
+
+        $transferance = $this->iugu->transfer()->create([
+            'receiver_id' => $client->id,
+            'amount_cents' => 100,
+        ]);
+
+        $this->assertEquals($transferance->amount_cents, 100);
+        $this->assertEquals($transferance->receiver->id, $client->id);
+    }
+}
