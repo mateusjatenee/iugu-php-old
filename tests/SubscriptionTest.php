11f09a57eb419c1cea54b858308e0d1d5ca62813
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-09 20:12:31 -0300

    Subscription test - test subscription can be created

---
+++ tests/SubscriptionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Iugu\Iugu;
+
+class SubscriptionTest extends PHPUnit_Framework_TestCase
+{
+    public function __construct()
+    {
+        $this->api_key = getenv('IUGU_API_KEY');
+        $this->account_id = getenv('IUGU_ACCOUNT_ID');
+        $this->iugu = new Iugu($this->api_key);
+
+    }
+
+    /**
+     * @group subscription
+     */
+    public function testSubscriptionCanBeCreated()
+    {
+        $client = $this->iugu->customer()->create([
+            'email' => 'John Doe',
+            'name' => 'email@email.com',
+            'notes' => 'nenhuma',
+        ]);
+
+        $subscription = $this->iugu->subscription()->create([
+            'customer_id' => $client->id,
+            'subitems' => [
+                'description' => 'Item um',
+                'quantity' => '1',
+                'price_cents' => '1000',
+            ],
+        ]);
+
+        $this->assertEquals($subscription->customer_name, $client->name);
+        $this->assertEquals($subscription->customer_id, $client->id);
+
+    }
+}
