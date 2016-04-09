cababb6bd5b60747a295679e4ea5d04c21f8c5df
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-09 20:23:28 -0300

    subscription class

---
+++ src/Subscription.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Iugu;
+
+use Iugu\Util\Request;
+
+class Subscription extends Request
+{
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @param $apiKey
+     */
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+        parent::__construct();
+    }
+
+    public function create(array $data)
+    {
+        $req = $this->postRequest('subscriptions', $data, $this->apiKey);
+
+        return $req;
+    }
+
+}
